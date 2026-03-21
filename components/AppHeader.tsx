d499503dd0c3846fd4053186a48daeaaea3f992d
gervaism-afk <gervaism@gmail.com>
2026-03-21 14:25:39 -0400

    Update AppHeader.tsx

--- components/AppHeader.tsx
+++ components/AppHeader.tsx
@@ -1,23 +1,34 @@
 "use client";
 
 import Link from "next/link";
+import Image from "next/image";
 
 export default function AppHeader() {
   return (
     <header className="sfHeader">
       <div className="sfHeaderInner">
+
+        {/* LOGO + BRAND */}
         <Link href="/" className="sfBrand">
-          <span className="sfBrandMark">SF</span>
-          <span>ShadowFox Sports Cards</span>
+          <Image
+            src="/logo.png"
+            alt="ShadowFox Sports Cards"
+            width={42}
+            height={42}
+            className="sfLogo"
+          />
+          <span className="sfBrandText">ShadowFox Sports Cards</span>
         </Link>
 
+        {/* NAV */}
         <nav className="sfNav">
           <Link href="/scan" className="sfNavLink">Scan</Link>
           <Link href="/manual" className="sfNavLink">Add</Link>
           <Link href="/collection" className="sfNavLink">Collection</Link>
           <Link href="/analytics" className="sfNavLink">Analytics</Link>
-          <Link href="/admin" className="sfNavLink">Admin</Link>
+          <Link href="/admin" className="sfNavLink sfAdminBtn">Admin</Link>
         </nav>
+
       </div>
     </header>
   );
